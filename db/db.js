ebbad9a6cbcba18f5ae051c3defb75340e018465
makos <gyelmis@gmail.com>
2019-11-16 00:24:53 +0100

    Implements Articles CRUD

--- db/db.js
+++ db/db.js
@@ -7,9 +7,9 @@ const pgp = require("pg-promise")(initOptions);
 let cn = "";
 
 if (process.env.NODE_ENV == "test") {
-  cn = "postgres://postgres@localhost/testdb";
+  cn = "postgres://me:password@localhost:5432/testdb";
 } else {
-  //cn = "postgres://postgres@localhost/testdb";
+  cn = "postgres://postgres@localhost/testdb";
 }
 
 const db = pgp(cn);
