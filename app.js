367a5f70d6676423a243dee1ba0e6deb85ed8909
makos <gyelmis@gmail.com>
2019-11-15 23:29:36 +0100

    Implements Admin CRUD user accounts

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const userRouter = require("./routes/user");
-const postRouter = require("./routes/post");
-const categoryRouter = require("./routes/category");
-const gifRouter = require("./routes/gif");
 
 const app = express();
 
@@ -31,23 +28,6 @@ app.get("/", function(req, res) {
   res.status(200).send({ status: "success", message: "Welcome to Teamwork!" });
 });
 
-// app.get("/insert", function(req, res) {
-//   db.one(
-//     "INSERT INTO users(name,email,password,role) VALUES($1, $2, $3, $4) RETURNING id",
-//     ["John Doe", "email@test.com", "password", 1]
-//   )
-//     .then(data => {
-//       console.log(data); // print new user id;
-//     })
-//     .catch(error => {
-//       console.log("ERROR:", error); // print error;
-//     });
-//   res.send("Hello World!");
-// });
-
 app.use("/api/v1/", userRouter);
-app.use("/api/v1/", postRouter);
-app.use("/api/v1/", categoryRouter);
-app.use("/api/v1/", gifRouter);
 
 module.exports = app;
