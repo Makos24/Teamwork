2c4ddf9b0faa0443ece4b9b15328b53a3528b7dd
makos <gyelmis@gmail.com>
2019-11-18 08:42:53 +0100

    Implements Gif CRUD

--- db/seeder.js
+++ db/seeder.js
@@ -1,11 +1,14 @@
 const { db } = require("./db");
 
-db.one("INSERT INTO users(name,email,role,password) VALUES($1, $2, $3,$4)", [
-  "Super Admin",
-  "admin@team.com",
-  1,
-  "$2b$10$YiH3X3emeMoPzStyUDIF5eCJQwXNv/8oqlmepRFdoHYu5D5vYe2N6"
-])
+db.one(
+  "INSERT INTO users(name,email,role,password) VALUES($1, $2, $3,$4) RETURNING *",
+  [
+    "Super Admin",
+    "admin@team.com",
+    1,
+    "$2b$10$YiH3X3emeMoPzStyUDIF5eCJQwXNv/8oqlmepRFdoHYu5D5vYe2N6"
+  ]
+)
   .then(data => {
     console.log("Admin account successfully created");
   })
@@ -13,12 +16,15 @@ db.one("INSERT INTO users(name,email,role,password) VALUES($1, $2, $3,$4)", [
     console.log(error);
   });
 
-db.one("INSERT INTO users(name,email,role,password) VALUES($1, $2, $3,$4)", [
-  "Test Employee",
-  "employee1@team.com",
-  2,
-  "$2b$10$YiH3X3emeMoPzStyUDIF5eCJQwXNv/8oqlmepRFdoHYu5D5vYe2N6"
-])
+db.one(
+  "INSERT INTO users(name,email,role,password) VALUES($1, $2, $3,$4) RETURNING *",
+  [
+    "Test Employee",
+    "employee1@team.com",
+    2,
+    "$2b$10$YiH3X3emeMoPzStyUDIF5eCJQwXNv/8oqlmepRFdoHYu5D5vYe2N6"
+  ]
+)
   .then(data => {
     console.log("Employee account successfully created");
   })
