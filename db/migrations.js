42e402261fa5afed23a6b892aa6a1b62b6d166a8
makos <gyelmis@gmail.com>
2019-11-18 08:38:26 +0100

    Implements Gif CRUD

--- db/migrations.js
+++ db/migrations.js
@@ -1,20 +1,19 @@
 const { db } = require("./db");
 
 db.none(
-  "CREATE TABLE IF NOT EXISTS session(sessionguid UUID NOT NULL, created text NOT NULL, sessionlife integer NOT NULL)"
-)
-  .then(data => {
-    console.log("successfully created");
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
-db.none(
-  "CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, email text UNIQUE NOT NULL, role integer NOT NULL, created_at timestamp default current_timestamp)"
+  "CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, email text UNIQUE NOT NULL, password text NOT NULL, role integer NOT NULL, created_at timestamp default current_timestamp)"
 )
   .then(data => {
     console.log("successfully created users table");
+    db.none(
+      "CREATE TABLE IF NOT EXISTS articles(id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL, body text NOT NULL, user_id INTEGER REFERENCES users(id), category_id INTEGER REFERENCES categories(id), created_at timestamp default current_timestamp)"
+    )
+      .then(data => {
+        console.log("successfully created articles table");
+      })
+      .catch(error => {
+        console.log(error);
+      });
   })
   .catch(error => {
     console.log(error);
@@ -31,10 +30,10 @@ db.none(
   });
 
 db.none(
-  "CREATE TABLE IF NOT EXISTS articles(id SERIAL PRIMARY KEY, title VARCHAR(255) NOT NULL, body text NOT NULL, user_id INTEGER REFERENCES users(id), category_id INTEGER REFERENCES categories(id), created_at timestamp default current_timestamp)"
+  "CREATE TABLE IF NOT EXISTS comments(id SERIAL PRIMARY KEY, body text NOT NULL, post_id INTEGER NOT NULL, post_type INTEGER NOT NULL, user_id INTEGER REFERENCES users(id), flag BOOLEAN default false, created_at timestamp default current_timestamp)"
 )
   .then(data => {
-    console.log("successfully created articles table");
+    console.log("successfully created comments table");
   })
   .catch(error => {
     console.log(error);
@@ -45,16 +44,6 @@ db.none(
 )
   .then(data => {
     console.log("successfully created gifs table");
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
-db.none(
-  "CREATE TABLE IF NOT EXISTS comments(id SERIAL PRIMARY KEY, body text NOT NULL, post_id INTEGER NOT NULL, post_type INTEGER NOT NULL, user_id INTEGER REFERENCES users(id), flag BOOLEAN default false, created_at timestamp default current_timestamp)"
-)
-  .then(data => {
-    console.log("successfully created comments table");
   })
   .catch(error => {
     console.log(error);
